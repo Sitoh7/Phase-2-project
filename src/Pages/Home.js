571b872fa13088ebefbcf262541bb1e3126d2ddc
Sitoh7 <ha.wakhule@gmail.com>
2024-11-11 10:23:28 +0300

    added navbar component

---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+import { Outlet, useOutletContext } from "react-router-dom"
+
+function Home(){
+    return(<>
+    <h1>Home</h1>
+   
+    </>)
+}
+
+export default Home
---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+
+
+function NavBar(){
+    return(<>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+  </div>
+  </div>
+  </div>
+  
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link"  href="#">Check Out</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Wish List</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Account</a>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+    </>)
+}
+
+export default NavBar
